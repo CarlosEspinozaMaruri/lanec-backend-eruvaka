4a8ba1e238a0a92f743f3ad9c7fd321f2be0ec9f
Jean Carlos Espinoza <j.c.e.m.23@gmail.com>
2022-03-25 14:20:49 -0500

    catcha

--- controller/eruvakaController.js
+++ controller/eruvakaController.js
@@ -4,60 +4,68 @@ let date = new Date().toJSON().split("T")[0];
 let time = new Date().toLocaleTimeString();
 
 export const eruvakaThief = async () => {
-  let accessToken = (
-    await axios.post("https://user-api.pondlogs.com/1.0/signin", {
-      email: "soportesistemas@corporacionlanec.com",
-      password: "Lanec.2020",
-      meta_data: {
-        browser:
-          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36",
-        client: "WEB",
-      },
-    })
-  ).data.access_token;
+  try {
+    let accessToken = (
+      await axios.post("https://user-api.pondlogs.com/1.0/signin", {
+        email: "soportesistemas@corporacionlanec.com",
+        password: "Lanec.2020",
+        meta_data: {
+          browser:
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36",
+          client: "WEB",
+        },
+      })
+    ).data.access_token;
 
-  letsGetInformation(accessToken);
+    letsGetInformation(accessToken);
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
 };
 
 const letsGetInformation = async (accessToken) => {
-  let config = {
-    headers: {
-      Authorization: accessToken,
-    },
-  };
+  try {
+    let config = {
+      headers: {
+        Authorization: accessToken,
+      },
+    };
 
-  let idsList = (
-    await axios.get(
-      "https://user-api.pondlogs.com/1.0/ponds/active/summary?location_id=5f74ada32bc16040de09c66e&get_all=true",
-      config
-    )
-  ).data.ponds;
+    let idsList = (
+      await axios.get(
+        "https://user-api.pondlogs.com/1.0/ponds/active/summary?location_id=5f74ada32bc16040de09c66e&get_all=true",
+        config
+      )
+    ).data.ponds;
 
-  let oxygenAndTempList = (
-    await axios.get(
-      "https://user-api.pondlogs.com/1.0/ponds/do-temperature?location_id=5f74ada32bc16040de09c66e&exclude_previous_harvest_data=true",
-      config
-    )
-  ).data.pond_water_quality_summary;
+    let oxygenAndTempList = (
+      await axios.get(
+        "https://user-api.pondlogs.com/1.0/ponds/do-temperature?location_id=5f74ada32bc16040de09c66e&exclude_previous_harvest_data=true",
+        config
+      )
+    ).data.pond_water_quality_summary;
 
-  let oxygenAndTempObject = [];
+    let oxygenAndTempObject = [];
 
-  idsList.forEach((elementId) => {
-    oxygenAndTempList.forEach((element) => {
-      if (element._id === elementId._id) {
-        oxygenAndTempObject.push({
-          serialDispositivo: "",
-          timestamp: `${date} ${time}`,
-          oxigenomgl: element.data[0].dissolved_oxygen,
-          oxigenoPorcentaje: 0,
-          temperatura: element.data[0].temperature,
-          bateria: 0,
-          nameHuman: elementId.title,
-        });
-      }
+    idsList.forEach((elementId) => {
+      oxygenAndTempList.forEach((element) => {
+        if (element._id === elementId._id) {
+          oxygenAndTempObject.push({
+            serialDispositivo: "",
+            timestamp: `${date} ${time}`,
+            oxigenomgl: element.data[0].dissolved_oxygen,
+            oxigenoPorcentaje: 0,
+            temperatura: element.data[0].temperature,
+            bateria: 0,
+            nameHuman: elementId.title,
+          });
+        }
+      });
     });
-  });
-  postData({ datos: oxygenAndTempObject });
+    postData({ datos: oxygenAndTempObject });
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
 };
 
 const postData = async (params) => {
@@ -83,9 +91,9 @@ const postData = async (params) => {
       params,
       config
     );
+
+    console.log(prueba.data);
   } catch (error) {
     console.log(`Error: ${error}`);
   }
-
-  console.log(prueba.data);
 };
